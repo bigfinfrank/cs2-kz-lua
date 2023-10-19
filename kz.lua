6319032857c5bc5a4f1da16f35beecf1a161d293
Fin <18708515+bigfinfrank@users.noreply.github.com>
2023-10-19 14:36:34 -0400

    remove-console-commands

--- kz.lua
+++ kz.lua
@@ -63,74 +63,14 @@ end
 
 function CvarsKztimer()
 	mode = 0
-	SendToServerConsole("sv_accelerate 6.5")
-	SendToServerConsole("sv_accelerate_use_weapon_speed 0")
-	SendToServerConsole("sv_airaccelerate 100.0")
-	SendToServerConsole("sv_air_max_wishspeed 30.0")
-	SendToServerConsole("sv_enablebunnyhopping 1")
-	SendToServerConsole("sv_friction 5.0")
-	SendToServerConsole("sv_gravity 800.0")
-	SendToServerConsole("sv_jump_impulse 301.993377")
-	SendToServerConsole("sv_ladder_scale_speed 1.0")
-	SendToServerConsole("sv_maxspeed 320.0")
-	SendToServerConsole("sv_maxvelocity 2000.0")
-	SendToServerConsole("sv_staminajumpcost 0.0")
-	SendToServerConsole("sv_staminalandcost 0.0")
-	SendToServerConsole("sv_staminamax 0.0")
-	SendToServerConsole("sv_staminarecoveryrate 0.0")
-	SendToServerConsole("sv_timebetweenducks 0.0")
-	SendToServerConsole("sv_wateraccelerate 10.0")
-	SendToServerConsole("mp_damage_headshot_only 1")
-	SendToServerConsole("mp_damage_scale_t_head 0")
-	SendToServerConsole("mp_damage_scale_ct_head 0")
 end
 
 function CvarsVanilla()
 	mode = 1
-	SendToServerConsole("sv_accelerate 5.5")
-	SendToServerConsole("sv_accelerate_use_weapon_speed 1")
-	SendToServerConsole("sv_airaccelerate 12.0")
-	SendToServerConsole("sv_air_max_wishspeed 30.0")
-	SendToServerConsole("sv_enablebunnyhopping 1")
-	SendToServerConsole("sv_friction 5.2")
-	SendToServerConsole("sv_gravity 800.0")
-	SendToServerConsole("sv_jump_impulse 301.993377")
-	SendToServerConsole("sv_ladder_scale_speed 0.78")
-	SendToServerConsole("sv_maxspeed 320.0")
-	SendToServerConsole("sv_maxvelocity 3500.0")
-	SendToServerConsole("sv_staminajumpcost 0.08")
-	SendToServerConsole("sv_staminalandcost 0.05")
-	SendToServerConsole("sv_staminamax 80.0")
-	SendToServerConsole("sv_staminarecoveryrate 60.0")
-	SendToServerConsole("sv_timebetweenducks 0.4")
-	SendToServerConsole("sv_wateraccelerate 10.0")
-	SendToServerConsole("mp_damage_headshot_only 1")
-	SendToServerConsole("mp_damage_scale_t_head 0")
-	SendToServerConsole("mp_damage_scale_ct_head 0")
 end
 
 function CvarsDefault()
 	mode = 1
-	SendToServerConsole("sv_accelerate 5.5")
-	SendToServerConsole("sv_accelerate_use_weapon_speed 1")
-	SendToServerConsole("sv_airaccelerate 12.0")
-	SendToServerConsole("sv_air_max_wishspeed 30.0")
-	SendToServerConsole("sv_enablebunnyhopping 0")
-	SendToServerConsole("sv_friction 5.2")
-	SendToServerConsole("sv_gravity 800.0")
-	SendToServerConsole("sv_jump_impulse 301.993377")
-	SendToServerConsole("sv_ladder_scale_speed 0.78")
-	SendToServerConsole("sv_maxspeed 320.0")
-	SendToServerConsole("sv_maxvelocity 3500.0")
-	SendToServerConsole("sv_staminajumpcost 0.08")
-	SendToServerConsole("sv_staminalandcost 0.05")
-	SendToServerConsole("sv_staminamax 80.0")
-	SendToServerConsole("sv_staminarecoveryrate 60.0")
-	SendToServerConsole("sv_timebetweenducks 0.4")
-	SendToServerConsole("sv_wateraccelerate 10.0")
-	SendToServerConsole("mp_damage_headshot_only 1")
-	SendToServerConsole("mp_damage_scale_t_head 0")
-	SendToServerConsole("mp_damage_scale_ct_head 0")
 end
 
 function locals()
@@ -190,9 +130,8 @@ local knifeCommands = {
 Convars:RegisterCommand("knife", function(_, knife)
     local command = knifeCommands[knife]
     if command then
-        SendToServerConsole("sv_cheats 1")
+        SendToServerConsole("sv_cheats true")
         SendToServerConsole("subclass_change " .. command .. " weapon_knife")
-        SendToServerConsole("sv_cheats 0")
     end
 end, nil, 0)
 
@@ -311,78 +250,6 @@ function Tick()
 end
 
 function Activate()
-	SendToServerConsole("sv_cheats 1")
-	SendToServerConsole("mp_ct_default_secondary weapon_usp_silencer")
-	SendToServerConsole("mp_t_default_secondary weapon_usp_silencer")
-	
-	SendToServerConsole("sv_holiday_mode 0")
-	SendToServerConsole("sv_party_mode 0")
-	
-	SendToServerConsole("sv_clamp_unsafe_velocities 0")
-	SendToServerConsole("mp_respawn_on_death_ct 1")
-	SendToServerConsole("mp_respawn_on_death_t 1")
-	SendToServerConsole("mp_respawn_immunitytime -1")
-	SendToServerConsole("sv_spec_post_death_additional_time 1")
-	
-	-- Hide money
-	SendToServerConsole("mp_playercashawards 0")
-	SendToServerConsole("mp_teamcashawards 0")
-	
-	-- Stop dropping guns
-	SendToServerConsole("mp_death_drop_c4 0")
-	SendToServerConsole("mp_death_drop_defuser 0")
-	SendToServerConsole("mp_death_drop_grenade 0")
-	SendToServerConsole("mp_death_drop_gun 1")
-	SendToServerConsole("mp_drop_knife_enable 1")
-	SendToServerConsole("mp_weapons_allow_map_placed 1")
-	SendToServerConsole("mp_disconnect_kills_players 0")
-	
-	-- No limits on joining teams
-	SendToServerConsole("mp_autoteambalance 0")
-	SendToServerConsole("mp_limitteams 0")
-	SendToServerConsole("mp_spectators_max 64")
-	
-	-- Performance
-	SendToServerConsole("sv_occlude_players 0")
-	
-	-- General
-	SendToServerConsole("sv_pure 0")
-	SendToServerConsole("sv_allow_votes 0")
-	SendToServerConsole("sv_infinite_ammo 2")
-	SendToServerConsole("mp_free_armor 2")
-	SendToServerConsole("mp_buytime 0")
-	SendToServerConsole("mp_freezetime 0")
-	SendToServerConsole("mp_team_intro_time 0")
-	SendToServerConsole("mp_ignore_round_win_conditions 1")
-	SendToServerConsole("mp_match_end_changelevel 1")
-	SendToServerConsole("sv_ignoregrenaderadio 1")
-	SendToServerConsole("sv_disable_radar 1")
-	SendToServerConsole("mp_footsteps_serverside 1")
-	SendToServerConsole("mp_warmuptime_all_players_connected 0")
-	SendToServerConsole("mp_maxmoney 100000")
-	SendToServerConsole("mp_startmoney 100000")
-	SendToServerConsole("mp_afterroundmoney 100000")
-	SendToServerConsole("mp_buytime 9999")
-	SendToServerConsole("mp_buy_anywhere 1")
-	SendToServerConsole("sv_infinite_ammo 1")
-	SendToServerConsole("mp_damage_scale_ct_body 0")
-	SendToServerConsole("mp_damage_scale_t_body 0")
-	SendToServerConsole("mp_damage_scale_ct_head 0")
-	SendToServerConsole("mp_damage_scale_t_head 0")
-	SendToServerConsole("mp_humanteam T")
-	SendToServerConsole("mp_give_player_c4 0")
-	-- Fix bots not spawning
-	SendToServerConsole("mp_randomspawn 1")
-	
-	-- Team picking
-	SendToServerConsole("mp_force_pick_time 60")
-	
-	-- End to falldamage
-	SendToServerConsole("sv_falldamage_scale 0")
-	
-	SendToServerConsole("mp_warmup_end")
-	SendToServerConsole("sv_cheats 0")
-
 	if mvmntSettings == 0 then
 		CvarsKztimer()
 	end
